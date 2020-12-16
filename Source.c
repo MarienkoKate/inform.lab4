3c8cd38a8e9ba3a021fbd34fb0330ad27b5d740d
MarienkoKate <71318441+MarienkoKate@users.noreply.github.com>
2020-12-16 15:21:23 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,34 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int func(char* str) {
-	int i, g;
+void func(char* str, char* mas) {
+	int i, g=0;
 	for (i = 0; str[i] != '\n'; i++) {
-			if ((str[i] < '0') || (str[i] > '9')) printf("%c", str[i]);
-			else if (str[i] == '1') printf("one");
-			else if (str[i] == '2') printf("two");
-			else if (str[i] == '3') printf("three");
-			else if (str[i] == '4') printf("four");
-			else if (str[i] == '5') printf("five");
-			else if (str[i] == '6') printf("six");
-			else if (str[i] == '7') printf("seven");
-			else if (str[i] == '8') printf("eight");
-			else if (str[i] == '9') printf("nine");
-			else if (str[i] == '0') printf("zero");
+		if ((str[i] < '0') || (str[i] > '9')) {
+			sprintf(mas+g, "%c", str[i]);
+			g++;
+		}
+		else if (str[i] == '1') {
+			sprintf(mas+g, "%s", "|one|");
+			g = g + 5;
+		}
+		else if (str[i] == '2') {
+			sprintf(mas + g, "%s", "|two|");
+			g = g + 5;
+		}
+		else if (str[i] == '3') {
+			sprintf(mas + g, "%s", "|three|");
+			g = g + 7;
+		}
+	    else if (str[i] == '4') {
+			sprintf(mas+g, "%s", "|four|");
+			g = g + 6;
+		}
+		else if (str[i] == '5') {
+			sprintf(mas+g, "%s", "|five|");
+			g = g + 6;
+		}
+		else if (str[i] == '6') {
+			sprintf(mas+g, "%s", "|six|");
+			g = g + 5;
+		}
+		else if (str[i] == '7') {
+			sprintf(mas+g, "%s", "|seven|");
+		    g = g + 7;
+		}
+	    else if (str[i] == '8') {
+			sprintf(mas+g, "%s", "|eight|");
+			g = g + 7;
+		}
+		else if (str[i] == '9') {
+			sprintf(mas+g, "%s", "|nine|");
+			g = g + 6;
+		}
+		else if (str[i] == '0') {
+			sprintf(mas+g, "%s", "|zero|");
+			g = g + 6;
+		}
 	}
 }
 int main() {
-	char ch = 0;
-	int v = 0, n = 5;
-	char* str = (char*)malloc(n * sizeof(char));
-	do {
-		scanf("%c", &ch);
-		str[v] = ch;
-		v++;
-		if (v == n - 2) {
-			n *= 2;
-			realloc(str, n * sizeof(char));
-		}
-	} while (ch != '\n');
-	func(str);
+	char str[1000];
+	fgets(str, 1000, stdin);
+	char mas[1000];
+	func(str, mas);
+	printf("%s", mas);
+	return 0;
 }

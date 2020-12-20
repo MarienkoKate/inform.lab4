1b484c4c6d2f113cf0c5fd41dfe3aed632d866f0
MarienkoKate <71318441+MarienkoKate@users.noreply.github.com>
2020-12-20 23:43:42 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,60 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-void func(char* str, char* mas) {
-	int i, g=0;
+int once(char* str, int i) {
+	if ((str[i - 1] == ' ' || i == 0) && (str[i + 1] == ' ' || str[i + 1] == '\n')) return 1;
+}
+void replacement(char* str, char* mas) {
+	int i, g = 0, sm = 0, numb = 0;
+	char* x[10];
+	x[0] = "zero";
+	x[1] = "one";
+	x[2] = "two";
+	x[3] = "three";
+	x[4] = "four";
+	x[5] = "five";
+	x[6] = "six";
+	x[7] = "seven";
+	x[8] = "eight";
+	x[9] = "nine";
 	for (i = 0; str[i] != '\n'; i++) {
-		if ((str[i] < '0') || (str[i] > '9')) {
-			sprintf(mas+g, "%c", str[i]);
-			g++;
-		}
-		else if (str[i] == '1') {
-			sprintf(mas+g, "%s", "|one|");
-			g = g + 5;
-		}
-		else if (str[i] == '2') {
-			sprintf(mas + g, "%s", "|two|");
-			g = g + 5;
-		}
-		else if (str[i] == '3') {
-			sprintf(mas + g, "%s", "|three|");
-			g = g + 7;
-		}
-	    else if (str[i] == '4') {
-			sprintf(mas+g, "%s", "|four|");
-			g = g + 6;
-		}
-		else if (str[i] == '5') {
-			sprintf(mas+g, "%s", "|five|");
-			g = g + 6;
-		}
-		else if (str[i] == '6') {
-			sprintf(mas+g, "%s", "|six|");
-			g = g + 5;
-		}
-		else if (str[i] == '7') {
-			sprintf(mas+g, "%s", "|seven|");
-		    g = g + 7;
-		}
-	    else if (str[i] == '8') {
-			sprintf(mas+g, "%s", "|eight|");
-			g = g + 7;
-		}
-		else if (str[i] == '9') {
-			sprintf(mas+g, "%s", "|nine|");
-			g = g + 6;
-		}
-		else if (str[i] == '0') {
-			sprintf(mas+g, "%s", "|zero|");
-			g = g + 6;
+		for (numb = -1; numb != 10; numb++) {
+			if ((str[i] < '0') || (str[i] > '9') || (int)(str[i] - '0') == numb && once(str, i) != 1) {
+				sprintf(mas + g, "%c", str[i]);
+				g++;
+				break;
+			}
+			else if ((int)(str[i] - '0') == numb && once(str, i) == 1) {
+				sprintf(mas + g, "%s", x[numb]);
+				g += strlen(x[numb]);
+				break;
+			}
 		}
 	}
 }
+int strlen(char* mas) {
+	int i = 0;
+	for (i; mas[i] != '\0'; i++);
+	return i;
+}
 int main() {
+	printf("imput string: ");
 	char str[1000];
 	fgets(str, 1000, stdin);
-	char mas[1000];
-	func(str, mas);
-	printf("%s", mas);
+	int len = strlen(str);
+	int i = len * 5;
+	char* mas = (char*)malloc(i * sizeof(char));
+	if (len != 0) {
+		replacement(str, mas);
+		printf("Result: %s", mas);
+	}
+	else printf("empty string ");
 	return 0;
+	free(mas);
 }
